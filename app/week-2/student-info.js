5956fa4b2c7730df60d70b65859b12dc061ac4b0
Nathanael Lee <nathanael.lee@edu.sait.ca>
2025-09-12 15:43:20 -0600

    changes for assignment 2 (changed home page)

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Nathanael Lee</h1>
+      <Link href="https://github.com/Darkorbi2/cprg306-assignments">
+        github link
+      </Link>
+    </main>
+  );
+}
